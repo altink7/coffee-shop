f8bfd077d2c0a8990917205f4e5036340d3a6c5d
altink7 <kelmendialtin48@gmail.com>
2025-05-27 10:07:13 +0200

    #4: db setup fix

--- backend/docker/postgres/init/init_db.sql
+++ backend/docker/postgres/init/init_db.sql
@@ -2,5 +2,5 @@
 DROP DATABASE IF EXISTS coffee_shop_db;
 CREATE DATABASE coffee_shop_db;
 
-\c transaction_service_db;
+\c coffee_shop_db;
 --jdbc:postgresql://localhost:5452/coffee_shop_db?user=root&password=root
